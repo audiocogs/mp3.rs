1a1cff591e595b65e4889ca5fa290b206088bda0
Jens Nockert <jens@nockert.se>
2014-09-06 04:58:48 +0200

    Slightly closer

--- bitreader.rs
+++ bitreader.rs
@@ -17,6 +17,8 @@ impl<'a> BitReader<'a> {
     if n <= self.cache_length {
       let result = self.cache >> (self.cache_length - n);
 
+      println!("Cached {:x}/{}", self.cache, self.cache_length);
+
       self.cache_length -= n;
       self.cache = self.cache & (0xFF >> (8 - self.cache_length));
 
@@ -28,6 +30,8 @@ impl<'a> BitReader<'a> {
       let read = match self.reader.read_be_uint_n(b_to_read) { Ok(n) => n, Err(e) => return Err(e) };
 
       let sum = ((self.cache << (b_to_read * 8)) as u64) | (read as u64);
+
+      println!("cached {:x}/{}/{:x}", self.cache, self.cache_length, sum);
 
       self.cache_length = b_to_read * 8 - n_to_read;
 
@@ -95,4 +99,16 @@ fn test_stream() {
   assert_eq!(r.read_bits(4).unwrap(), 0xD);
   assert_eq!(r.read_bits(4).unwrap(), 0x2);
   assert_eq!(r.read_bits(4).unwrap(), 0x1);
+}
+
+#[test]
+fn test_stream2() {
+  let buf = [0x30, 0xC8, 0x61, 0xA6, 0x9A, 0xAA];
+  let mut br = io::BufReader::new(buf);
+  let mut r = BitReader::new(&mut br);
+
+  assert_eq!(r.read_bits(6).unwrap(), 12);
+  assert_eq!(r.read_bits(6).unwrap(), 12);
+  assert_eq!(r.read_bits(6).unwrap(), 33);
+  assert_eq!(r.read_bits(6).unwrap(), 33);
 }
--- layer1.rs
+++ layer1.rs
@@ -45,46 +45,32 @@ pub fn decode_layer1(reader: &mut io::fs::File, frame_header: header::Header) ->
   samples
 }
 
-fn decode_bit_allocations(bit_reader: &mut bitreader::BitReader, num_subbands: uint, num_channels: uint) -> Box<[[u32, ..32], ..2]>{
+fn decode_bit_allocations(bit_reader: &mut bitreader::BitReader, num_subbands: uint, num_channels: uint) -> Box<[[u32, ..32], ..2]> {
   let mut allocations = box [[0u32, ..32], ..2];
 
   for subband in range(0, num_subbands) {
     for channel in range(0, num_channels) {
       let g = bit_reader.read_bits(4);
 
-      let n = match g {
-        Ok(0) => 0,
-        Ok(15) => fail!("illegal bit value"),
-        Ok(n) => n + 1,
-        Err(_) => fail!("illegal bitz value")
-      };
-
-      println!("{:x}", g.unwrap());
-
-      allocations[channel][subband] = n;
+      allocations[channel][subband] = g.unwrap();
     }
   }
 
   return allocations;
 }
 
-fn decode_scale_factors(bit_reader: &mut bitreader::BitReader, nb_subbands: uint, num_channels: uint, allocations: &Box<[[u32, ..32], ..2]>) -> Box<[[u32, ..32], ..2]> {
+fn decode_scale_factors(bit_reader: &mut bitreader::BitReader, num_subbands: uint, num_channels: uint, allocations: &Box<[[u32, ..32], ..2]>) -> Box<[[u32, ..32], ..2]> {
   let mut scale_factors = box [[0u32, ..32], ..2];
-  let n_bits_to_read = 6;
 
-  for chan in range(0, num_channels) {
-      for i in range(0, nb_subbands) {
-        let factor = if allocations[chan][i] != 0 {
-          match bit_reader.read_bits(n_bits_to_read) {
-            Ok(n) => n,
-            Err(_) => 0
-          }
-        }else{ 0 };
-        scale_factors[chan][i] = factor;
-      }
+  for subband in range(0, num_subbands) {
+    for channel in range(0, num_channels) {
+      let factor = if allocations[channel][subband] != 0 { bit_reader.read_bits(6).unwrap() } else { 0 };
+
+      scale_factors[channel][subband] = factor;
+    }
   }
 
-  scale_factors
+  return scale_factors;
 }
 
 fn decode_samples(bit_reader: &mut bitreader::BitReader, nb_subbands: uint, num_channels: uint, allocations: &Box<[[u32, ..32], ..2]>, scale_factors: &Box<[[u32, ..32], ..2]>) -> Box<[[[f64, ..32], ..12], ..2]> {
@@ -128,3 +114,157 @@ fn sample(bit_reader: &mut bitreader::BitReader, nb: uint) -> f64 {
 
 }
 
+fn generate_test_allocations() -> Box<[[u32, ..32], ..2]> {
+  let buf = [0xED, 0x99, 0x88, 0x88, 0x88, 0x88, 0x77, 0x77, 0x66, 0x77, 0x55, 0x66, 0x55, 0x55, 0x55, 0x55, 0x44, 0x44, 0x44, 0x33, 0x44, 0x22, 0x33, 0x22, 0x22, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+  let mut br = io::BufReader::new(buf);
+  let mut r = bitreader::BitReader::new(&mut br);
+
+  return decode_bit_allocations(&mut r, 32, 2);
+}
+
+#[test]
+fn test_bit_allocations() {
+  let samples = generate_test_allocations();
+
+  assert_eq!(samples[0][0], 0xE);
+  assert_eq!(samples[0][1], 0x9);
+  assert_eq!(samples[0][2], 0x8);
+  assert_eq!(samples[0][3], 0x8);
+  assert_eq!(samples[0][4], 0x8);
+  assert_eq!(samples[0][5], 0x8);
+  assert_eq!(samples[0][6], 0x7);
+  assert_eq!(samples[0][7], 0x7);
+  assert_eq!(samples[0][8], 0x6);
+  assert_eq!(samples[0][9], 0x7);
+  assert_eq!(samples[0][10], 0x5);
+  assert_eq!(samples[0][11], 0x6);
+  assert_eq!(samples[0][12], 0x5);
+  assert_eq!(samples[0][13], 0x5);
+  assert_eq!(samples[0][14], 0x5);
+  assert_eq!(samples[0][15], 0x5);
+  assert_eq!(samples[0][16], 0x4);
+  assert_eq!(samples[0][17], 0x4);
+  assert_eq!(samples[0][18], 0x4);
+  assert_eq!(samples[0][19], 0x3);
+  assert_eq!(samples[0][20], 0x4);
+  assert_eq!(samples[0][21], 0x2);
+  assert_eq!(samples[0][22], 0x3);
+  assert_eq!(samples[0][23], 0x2);
+  assert_eq!(samples[0][24], 0x2);
+  assert_eq!(samples[0][25], 0x0);
+  assert_eq!(samples[0][26], 0x0);
+  assert_eq!(samples[0][27], 0x0);
+  assert_eq!(samples[0][28], 0x0);
+  assert_eq!(samples[0][29], 0x0);
+  assert_eq!(samples[0][30], 0x0);
+  assert_eq!(samples[0][31], 0x0);
+  
+  assert_eq!(samples[1][0], 0xD);
+  assert_eq!(samples[1][1], 0x9);
+  assert_eq!(samples[1][2], 0x8);
+  assert_eq!(samples[1][3], 0x8);
+  assert_eq!(samples[1][4], 0x8);
+  assert_eq!(samples[1][5], 0x8);
+  assert_eq!(samples[1][6], 0x7);
+  assert_eq!(samples[1][7], 0x7);
+  assert_eq!(samples[1][8], 0x6);
+  assert_eq!(samples[1][9], 0x7);
+  assert_eq!(samples[1][10], 0x5);
+  assert_eq!(samples[1][11], 0x6);
+  assert_eq!(samples[1][12], 0x5);
+  assert_eq!(samples[1][13], 0x5);
+  assert_eq!(samples[1][14], 0x5);
+  assert_eq!(samples[1][15], 0x5);
+  assert_eq!(samples[1][16], 0x4);
+  assert_eq!(samples[1][17], 0x4);
+  assert_eq!(samples[1][18], 0x4);
+  assert_eq!(samples[1][19], 0x3);
+  assert_eq!(samples[1][20], 0x4);
+  assert_eq!(samples[1][21], 0x2);
+  assert_eq!(samples[1][22], 0x3);
+  assert_eq!(samples[1][23], 0x2);
+  assert_eq!(samples[1][24], 0x2);
+  assert_eq!(samples[1][25], 0x1);
+  assert_eq!(samples[1][26], 0x0);
+  assert_eq!(samples[1][27], 0x0);
+  assert_eq!(samples[1][28], 0x0);
+  assert_eq!(samples[1][29], 0x0);
+  assert_eq!(samples[1][30], 0x0);
+  assert_eq!(samples[1][31], 0x0);
+}
+
+#[test]
+fn test_scale_factors() {
+  let allocations = generate_test_allocations();
+  
+  let buf = [0x30, 0xC8, 0x61, 0xA6, 0x9A, 0xAA, 0xBA, 0xEB, 0x6D, 0xCB, 0x2C, 0x30, 0xD3, 0x4C, 0xB2, 0xDB, 0x6D, 0x34, 0xE3, 0x8D, 0x75, 0xDF, 0x7D, 0xF7, 0xDF, 0x7E, 0x79, 0xDF, 0x7E, 0xBA, 0xDF, 0x7E, 0xBA, 0xE3, 0x8E, 0xBA, 0xE3, 0x8E, 0xDF, 0xFF, 0xBF, 0xFE, 0xFF, 0xBF, 0xEF, 0xF7, 0xFB, 0xFD];
+  let mut br = io::BufReader::new(buf);
+  let mut r = bitreader::BitReader::new(&mut br);
+
+  let samples = decode_scale_factors(&mut r, 32, 2, &allocations);
+
+  assert_eq!(samples[0][0], 12);
+  assert_eq!(samples[1][0], 12);
+  assert_eq!(samples[0][1], 33);
+  assert_eq!(samples[1][1], 33);
+  assert_eq!(samples[0][2], 41);
+  assert_eq!(samples[1][2], 41);
+  assert_eq!(samples[0][3], 42);
+  assert_eq!(samples[1][3], 42);
+  assert_eq!(samples[0][4], 46);
+  assert_eq!(samples[1][4], 46);
+  assert_eq!(samples[0][5], 45);
+  assert_eq!(samples[1][5], 45);
+  assert_eq!(samples[0][6], 50);
+  assert_eq!(samples[1][6], 50);
+  assert_eq!(samples[0][7], 48);
+  assert_eq!(samples[1][7], 48);
+  assert_eq!(samples[0][8], 52);
+  assert_eq!(samples[1][8], 52);
+  assert_eq!(samples[0][9], 50);
+  assert_eq!(samples[1][9], 50);
+  assert_eq!(samples[0][10], 54);
+  assert_eq!(samples[1][10], 54);
+  assert_eq!(samples[0][11], 52);
+  assert_eq!(samples[1][11], 52);
+  assert_eq!(samples[0][12], 56);
+  assert_eq!(samples[1][12], 56);
+  assert_eq!(samples[0][13], 53);
+  assert_eq!(samples[1][13], 53);
+  assert_eq!(samples[0][14], 55);
+  assert_eq!(samples[1][14], 55);
+  assert_eq!(samples[0][15], 55);
+  assert_eq!(samples[1][15], 55);
+  assert_eq!(samples[0][16], 55);
+  assert_eq!(samples[1][16], 55);
+  assert_eq!(samples[0][17], 57);
+  assert_eq!(samples[1][17], 57);
+  assert_eq!(samples[0][18], 55);
+  assert_eq!(samples[1][18], 55);
+  assert_eq!(samples[0][19], 58);
+  assert_eq!(samples[1][19], 58);
+  assert_eq!(samples[0][20], 55);
+  assert_eq!(samples[1][20], 55);
+  assert_eq!(samples[0][21], 58);
+  assert_eq!(samples[1][21], 58);
+  assert_eq!(samples[0][22], 56);
+  assert_eq!(samples[1][22], 56);
+  assert_eq!(samples[0][23], 58);
+  assert_eq!(samples[1][23], 58);
+  assert_eq!(samples[0][24], 56);
+  assert_eq!(samples[1][24], 56);
+  assert_eq!(samples[0][25], 59);
+  assert_eq!(samples[1][25], 0);
+  assert_eq!(samples[0][26], 0);
+  assert_eq!(samples[1][26], 0);
+  assert_eq!(samples[0][27], 0);
+  assert_eq!(samples[1][27], 0);
+  assert_eq!(samples[0][28], 0);
+  assert_eq!(samples[1][28], 0);
+  assert_eq!(samples[0][29], 0);
+  assert_eq!(samples[1][29], 0);
+  assert_eq!(samples[0][30], 0);
+  assert_eq!(samples[1][30], 0);
+  assert_eq!(samples[0][31], 0);
+  assert_eq!(samples[1][31], 0);
+}
